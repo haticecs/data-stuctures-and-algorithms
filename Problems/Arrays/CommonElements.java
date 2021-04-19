be4ef41a60083072bb7a75bbd4cad82fb72d95c6
haticecs <57989556+haticecs@users.noreply.github.com>
2021-04-19 16:45:11 +0300

    Common element book solution added

--- Problems/Arrays/CommonElements.java
+++ Problems/Arrays/CommonElements.java
@@ -64,3 +64,27 @@ public class CommonElements {
     System.out.println();
   }
 }
+/* Solution
+public static Integer[] commonElements(int[] array1, int[] array2) {
+        int p1 = 0;
+        int p2 = 0;
+        // Need to use ArrayList because we don't know the size of the resulting
+        // array yet. Note that an ArrayList is resizable.
+        ArrayList<Integer> result = new ArrayList();
+        while(p1 < array1.length && p2 < array2.length){
+            if (array1[p1] == array2[p2]) {
+                result.add(array1[p1]);
+                p1 += 1;
+                p2 += 1;
+            }
+            else if (array1[p1] > array2[p2]) {
+                p2 += 1;
+            } else {
+                p1 += 1;
+            }
+        }
+        // Convert the result to a regular array.
+        Integer[] resultInArray = new Integer[result.size()];
+        return result.toArray(resultInArray);
+    }
+  */

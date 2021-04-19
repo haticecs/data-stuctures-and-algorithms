3e3606f1282a83f4f8bf4b2732da58e06abb8e49
haticecs <57989556+haticecs@users.noreply.github.com>
2021-04-19 23:36:04 +0300

    Solution is added

--- Problems/Arrays/ArrayRotation.java
+++ Problems/Arrays/ArrayRotation.java
@@ -78,3 +78,22 @@ public class ArrayRotation {
     return true;
   }
 }
+/*Solution
+public static Boolean isRotation(int[] array1, int[] array2) {
+        if (array1.length != array2.length) return false;
+        int key = array1[0];
+        int keyLoc = -1;
+        for (int i = 0; i < array2.length; i++) {
+            if (array2[i] == key) {
+                keyLoc = i;
+                break;
+            }
+        }
+        if (keyLoc == -1) return false;
+        for (int i = 0; i < array1.length; i++) {
+            int j = (keyLoc + i) % array1.length;
+            if (array1[i] != array2[j]) return false;
+        }
+        return true;
+    }
+    */

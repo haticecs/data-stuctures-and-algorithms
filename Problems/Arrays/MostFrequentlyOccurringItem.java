02c84cd081ae60274cc41083dfc26dcb01da3e05
haticecs <57989556+haticecs@users.noreply.github.com>
2021-04-19 16:23:09 +0300

    Solution added

--- Problems/Arrays/MostFrequentlyOccurringItem.java
+++ Problems/Arrays/MostFrequentlyOccurringItem.java
@@ -67,3 +67,22 @@ public class MostFrequentlyOccurringItem {
     return maxItem;
   }
 }
+/* Solution
+    public static Integer mostFreqent(int[] givenArray) {
+        Integer maxCount = -1; Integer maxItem = null;
+        HashMap<Integer, Integer> count = new HashMap<Integer, Integer>();
+        for (int i : givenArray) {
+            if (count.containsKey(i)) {
+                Integer newVal = count.get(i) + 1;
+                count.put(i, newVal);
+            } else {
+                count.put(i, 1);
+            }
+            if (count.get(i) > maxCount) {
+                maxCount = count.get(i);
+                maxItem = i;
+            }
+        }
+        return maxItem;
+    }
+*/

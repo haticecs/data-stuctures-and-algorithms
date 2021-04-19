6f238b224267007358a25697cbe844fe01422b6a
haticecs <57989556+haticecs@users.noreply.github.com>
2021-04-20 01:29:39 +0300

    Non Repeating Chracter Solution is added

--- Problems/Strings/NonRepeatingCharacter.java
+++ Problems/Strings/NonRepeatingCharacter.java
@@ -9,7 +9,7 @@ If there is no non-repeating character, return null.
 
 package Problems.Strings;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class NonRepeatingCharacter {
 
@@ -19,10 +19,48 @@ public class NonRepeatingCharacter {
     System.out.println(nonRepeating("abab")); // should return null
     System.out.println(nonRepeating("aabbbc")); // should return 'c'
     System.out.println(nonRepeating("aabbdbc")); // should return 'd'
+    System.out.println(nonRepeating("cbbabb")); // should return 'c'
   }
 
   // Implement your solution below.
   public static Character nonRepeating(String s) {
+    //LinkedHashMap maintains original insertion order, HashMap doesn't.
+    HashMap<Character, Integer> hm = new LinkedHashMap<>();
+    char[] arr = s.toCharArray();
+    for (char c : arr) {
+      //c is a current key
+      if (hm.containsKey(c)) {
+        Integer newCount = hm.get(c) + 1;
+        hm.put(c, newCount);
+      } else {
+        hm.put(c, 1);
+      }
+    }
+    for (Character k : hm.keySet()) {
+      int count = hm.get(k);
+      if (count == 1) {
+        return k;
+      }
+    }
     return null;
   }
 }
+/* Solution
+  public static Character nonRepeating2(String s) {
+    HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
+    // NOTE: Using s.toCharArray() is no the most efficient method,
+    // but I chose to use it here for simplicity.
+    for (char c : s.toCharArray()) {
+      if (charCount.containsKey(c)) {
+        Integer newVal = charCount.get(c) + 1;
+        charCount.put(c, newVal);
+      } else {
+        charCount.put(c, 1);
+      }
+    }
+    for (char c : s.toCharArray()) {
+      if (charCount.get(c) == 1) return c;
+    }
+    return null;
+  }
+*/
